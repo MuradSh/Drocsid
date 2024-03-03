8134808803062b97f1bccdea1d95c9c12a93d2d3
ammaarkhan <ammaarkhan03@gmail.com>
2024-03-03 13:37:05 -0800

    react base

---
+++ code/drocsid/src/pages/Login.js
@@ -0,0 +1,43 @@
+// Login.js
+import './Login.css';
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents the default form submit action (page reload)
+    // Here, you can handle the login logic, like calling an API to authenticate the user
+    console.log('Login Submitted', { email, password });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
