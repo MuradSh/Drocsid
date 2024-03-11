962d7479f14b7e6b679a111bcf4be9f7fc13b1b6
ammaarkhan <ammaarkhan03@gmail.com>
2024-03-11 07:35:29 -0700

    integrate firebase with react app

---
+++ code/drocsid/src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBv2i2zLOraCjjQknJBxPBfpNrGpYXHMu0",
+  authDomain: "drocsid-f50c2.firebaseapp.com",
+  projectId: "drocsid-f50c2",
+  storageBucket: "drocsid-f50c2.appspot.com",
+  messagingSenderId: "368092363998",
+  appId: "1:368092363998:web:bb0e42b145119a6f05164f",
+  measurementId: "G-JKE29196PY",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore();
+
+export { app, auth, firestore };
