07beb71215fd9d29f80e7ca81844bfcf44fc8fe5
Murad Shahmammadli <64356412+MuradSh@users.noreply.github.com>
2024-03-25 16:38:14 -0700

    FEAT: admin panel implemented

--- code/drocsid/src/pages/admin/admin.js
+++ code/drocsid/src/pages/admin/admin.js
@@ -5,9 +5,9 @@ import "./admin.css";
 import { useAuth } from "../../contexts/authContext"; // Adjust this path as necessary
 import { doSignOut } from "../../firebase/auth";
 import { firestore } from '../../firebase/firebase'; // Adjust this path as necessary
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, deleteDoc, updateDoc, addDoc, where, query } from 'firebase/firestore';
 
-const pages = ["Analytics", "Events", "Users"];
+const pages = ["Pending Decision", "All Events", "Users"];
 
 
 const Admin = () => {
@@ -16,21 +16,49 @@ const Admin = () => {
     const [password, setPassword] = useState("");
     const { userLoggedIn } = useAuth();
     const [pagez, setPagez] = useState("Analytics");
-    const [allItems, setAllItems] = useState([]);
+    const [showingElements, setShowingElements] = useState([]);
+    const [allElements, setAllElements] = useState([]);
+    const [pendingElements, setPendingElements] = useState([]);
 
     useEffect(() => {
-        const fetchItems = async () => {
-            const querySnapshot = await getDocs(collection(firestore, "events"));
-            const itemsArray = querySnapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            setAllItems(itemsArray);
-        };
         fetchItems().catch(console.error);
-        console.log(allItems);
+        if (window.location.hash != "") {
+            changePage(window.location.hash);
+        }
     }, []);
 
+
+
+    const fetchItems = async () => {
+        const querySnapshot = await getDocs(collection(firestore, "events"));
+        var pendingItemsArray = [];
+        for (let i = 0; i < querySnapshot.docs.length; i++) {
+            if (querySnapshot.docs[i].data().verified == null) {
+                pendingItemsArray.push({
+                    id: querySnapshot.docs[i].id,
+                    ...querySnapshot.docs[i].data(),
+                });
+            }
+        }
+        setPendingElements(pendingItemsArray);
+        const querySnapshot2 = await getDocs(collection(firestore, "events"));
+        var allItemsArray = querySnapshot2.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+        setAllElements(allItemsArray);
+        setShowingElements(pendingItemsArray);
+    };
+
+    // changin page from left bar
+    const changePage = (page) => {
+        setPagez(page);
+        if (page == "#pen") {
+            setShowingElements(pendingElements);
+        } else if (page == "#all") {
+            setShowingElements(allElements);
+        }
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -55,7 +83,34 @@ const Admin = () => {
     };
 
     const handleLiClick = (e) => {
-        setPagez(e.target.innerHTML);
+        var page = e.target.innerHTML.substring(0, 3).toLowerCase();
+        changePage("#" + page);
+        window.location.hash = page;
+    }
+
+    const handleDelete = async (e) => {
+        var id = e.target.getAttribute("data-id");
+
+        const eventDocRef = doc(firestore, 'events', id);
+        await deleteDoc(eventDocRef);
+        fetchItems(); // Refresh the list after deletion
+    }
+
+    const handleDecision = async (decision, id) => {
+        const eventDocRef = doc(firestore, 'events', id);
+        await updateDoc(eventDocRef, {
+            verified: decision
+        });
+
+        fetchItems(); // Refresh the list after checkbox change
+
+        await addDoc(collection(firestore, 'notifications'), {
+            seen: false,
+            event_id: id,
+            time: new Date(),
+            decision: decision
+        });
+        alert("Decision has been made!");
     }
 
     return (
@@ -104,25 +159,32 @@ const Admin = () => {
                     <tbody>
 
                         <tr className="table-header">
-                            {
-                                allItems.length > 0 ?
-
-                                    Object.keys(allItems[0]).map((key, index) => (
-                                        <th key={index}>{key}</th>
-                                    )) : null
-                            }
+                            <th>id</th>
+                            <th>category</th>
+                            <th>date</th>
+                            <th>venue</th>
+                            <th>name</th>
+                            <th>availability</th>
+                            <th>description</th>
+                            <th>pricing</th>
+                            <th>time</th>
                             <th>Actions</th>
                         </tr>
                         {
-                            allItems.map((item, index) => (
-                                <tr className="table-row" key={index}>
-                                    {
-                                        Object.keys(item).map((key, index) => (
-                                            <td className="table-data" key={index}>{item[key]}</td>
-                                        ))
-                                    }
-                                    <td>
-                                        <button className="deleteButton">Delete</button>
+                            showingElements.map((item, index) => (
+                                < tr className="table-row" key={index} >
+                                    <td className="table-data">{item.id}</td>
+                                    <td className="table-data">{item.category}</td>
+                                    <td className="table-data">{item.date}</td>
+                                    <td className="table-data">{item.venue}</td>
+                                    <td className="table-data">{item.name}</td>
+                                    <td className="table-data">{item.availability}</td>
+                                    <td className="table-data">{item.description}</td>
+                                    <td className="table-data">{item.pricing}</td>
+                                    <td className="table-data">{item.time}</td>
+                                    < td >
+                                        <button className="approveButton" onClick={() => handleDecision(true, item.id)}>Approve</button>
+                                        <button className="rejectButton" onClick={() => handleDecision(false, item.id)}>Reject</button>
                                     </td>
                                 </tr>
                             ))
@@ -130,7 +192,7 @@ const Admin = () => {
                         }
                     </tbody>
                 </table>
-            </div>
+            </div >
         </div >
     );
 };
